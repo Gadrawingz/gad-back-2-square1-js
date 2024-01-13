9f0e7885e32584e9f0b3e88572578c9643370518
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-01-13 22:53:09 +0200

    Last commit on sat 13th

---
+++ 01-easily-coded/operators.js
@@ -0,0 +1,11 @@
+// An operand: is what operators are applied to. 
+// UNARY: An operator is unary if it has a single operand. 
+// For example: The unary negation - reverses the sign of a number:
+let posNumber = 30;
+let negNumber = -posNumber;
+document.write('<br>', negNumber)
+
+// An operator is binary if it has two operands. 
+// The same minus exists in binary form as well:
+let x = 30, y = -10
+document.write('<br>', x - y)
