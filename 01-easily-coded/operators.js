a83d4069493cc489fdb6216b5b40f3069895d22d
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-01-15 22:34:55 +0200

    Second Commit On Tuesday on OP

--- 01-easily-coded/operators.js
+++ 01-easily-coded/operators.js
@@ -19,3 +19,48 @@ document.write("<br>", 5 ** 2)
 // NB2:(Power of 1/3 is the same as cubic root)
 document.write("<br>", 5 ** (1/3))
 document.write("<br>", 5 ** 2)
+
+
+// Assignment = returns a value:??
+let f = 68;
+let g = 4;
+let h = 3;
+let hh = 10 - (f = 5 + 3); // 10 - 8
+let gg = 30 - (g = h + 5); // 30 - 7
+// Here the call (g = ...) writes the value into g
+// Then returns it.
+
+// Thats for + and -
+let pp = 74 - (h = f + g - h) 
+document.write("<hr>", hh)
+document.write("<br>", gg)
+document.write("<br>", pp) // ??
+
+
+// Chaining Assignment
+// This evaluates from right to left
+let j1, ok, ya;
+j1 = ok = ya = 4 + 4;
+
+// Evaluate like this from 4 + 4 to ya then ok....
+document.write("<br>", j1) // 8
+document.write("<br>", ok) // 8
+document.write("<br>", ya) // 8
+
+// Modify-in-place
+// Here I apply an operator to a variable and store
+// the new result in that same value.
+let big = 24;
+
+big = big + 3; // 7, big as big = big + 7
+document.write("<br>", big) // 27
+
+let small = 14
+small += 2; // 16, same as small = small + 2
+document.write("<br>", small) // 16
+
+let num5 = 6;
+num5 *= 2 + 3; // same as num5 * 5, 6 * 5
+document.write("<br>", num5) // 30
+
+
