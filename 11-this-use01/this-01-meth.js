cb1a1c0de5804660bc1ef6e25af66eea6c5c5bc8
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-02-27 23:54:12 +0200

    Third commit on using this Tuesday!

--- 11-this-use01/this-01-meth.js
+++ 11-this-use01/this-01-meth.js
@@ -1,9 +1,11 @@
 // 1. First use case...
 let student = {
-    fullName : "Jhene Aiko",
+    fullName : "Yuen Biao",
     age : 48,
     gender : "Female",
 
+    // Code inside student.printDetails() 
+    // may need the name of the student
     printDetails() {
         document.write(`<br>Student data is ${this.fullName} ${this.age}<br>`);
     }
@@ -12,3 +14,16 @@ let student = {
 // Usage
 document.write("Gender: "+student.gender);
 student.printDetails();
+
+
+// 2nd way with function:
+const prisoner = {
+    firstName : "Jackie",
+    lastName: "Chan",
+    prisonerId : 4344,
+
+    getFullName : function() {
+        return this.firstName +" "+this.lastName;
+    }
+};
+
