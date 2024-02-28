77e817733e6ec8ef9e895d24129f23c1d323af52
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-02-28 23:05:21 +0200

    Second commit on object with prompt

--- 09-objects-01/objects-02.js
+++ 09-objects-01/objects-02.js
@@ -13,3 +13,22 @@ student2["has good vibes"] = true;
 document.write(student2["has good vibes"]); //true
 // delete
 // delete student2["has good vibes"];
+
+
+
+// 2. The variable key may be calculated at run-time or depend 
+// on the user input. And then we use it to access the property
+let player = {
+    fullName: "Darwin Nunez",
+    score: 40,
+    age : 24
+};
+
+let key = prompt("Anything you wanna know about player?","fullName");
+
+// Access by variable
+document.write("<br>");
+document.write(player[key]);
+document.write("<br>");
+
+
