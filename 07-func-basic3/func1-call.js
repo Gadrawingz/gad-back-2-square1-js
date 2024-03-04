9699cb04a422d8e823d3dae4bd9de82955269021
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-03-04 23:01:40 +0200

    Second commit on using call function

--- 07-func-basic3/func1-call.js
+++ 07-func-basic3/func1-call.js
@@ -15,7 +15,28 @@ function getProduct(x, y) {
     return x * y;
 }
 
+// Without using call()- we can directly invoke getProduct() 
 let res1 = getProduct(7, 3);
 document.write(`${res1}<br>`);
-let res2 = getProduct(4, 2); // 8
-document.write(`${res2}<br>`);
+
+// Invoking getProduct() using call() method
+let res2 = getProduct.call(this, 4, 2); // 8
+document.write(`${res2}<hr>`);
+
+// 03. Passing Object as this Value in call() method
+// Object definition
+const player = {
+    firstName: "Sadio",
+    lastName: "Mane",
+    playerAge: 30,
+};
+
+// Function definition
+function greet() {
+    const str = `My name is ${this.firstName} ${this.lastName}
+    . I am ${this.playerAge} years old`;
+    document.write(str);
+}
+// passing object as this value in call() method
+greet.call(player);
+
