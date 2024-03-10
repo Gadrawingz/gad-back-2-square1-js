c89f50ca64221153888d13fcca261bb1afd1cbcb
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-03-10 23:34:29 +0200

    First commit on Sunday, object prop copying...

---
+++ 09-objects-01/Object.assign.js
@@ -0,0 +1,12 @@
+/*
+This method Object.assign:
+
+Syntax: Object.assign(dest, ...sources)
+1.The 1st argument dest is a target object.
+2.The 2nd arguments is a list of source objects.
+
+It copies the properties of all source objects into the target dest, and then returns it as the result.
+*/
+
+
+// 01. I add couple of permissions to student object
