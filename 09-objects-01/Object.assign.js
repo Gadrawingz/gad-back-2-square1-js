6420929833ba74d0ab53f3b52e14b1a4e14d0e9f
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-03-10 23:44:34 +0200

    2nd commit on using Object.assign

--- 09-objects-01/Object.assign.js
+++ 09-objects-01/Object.assign.js
@@ -10,3 +10,31 @@ It copies the properties of all source objects into the target dest, and then re
 
 
 // 01. I add couple of permissions to student object
+let student = {
+    fullName : "Gad Iradufasha",
+    age: 78
+};
+
+let permission1 = { canView: true };
+let permission2 = { canDelete: false };
+let permission3 = { canModify: true };
+
+// copies all properties from permission1, persmission2
+// and permission3 into student
+Object.assign(student, permission1, permission2, permission3);
+console.log(student.fullName); // Gad Iradufasha
+console.log(student.age); // 78
+console.log(student.canView); // true
+console.log(student.canModify); // true
+console.log(student.canDelete); // false
+console.log(student)
+/*
+Same as:
+{
+  fullName: 'Gad Iradufasha',
+  age: 78,
+  canView: true,
+  canDelete: false,
+  canModify: true
+}
+*/
